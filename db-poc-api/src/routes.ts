6a20b176a5b72b017a613df886699982ff51b698
BraCR10 <brianramirez01arias@gmail.com>
2025-04-11 23:57:01 -0600

    Draft: Defining main routes

--- db-poc-api/src/routes.ts
+++ db-poc-api/src/routes.ts
@@ -12,9 +12,25 @@ router.get("/health", (req, res) => {
   res.status(200).json({ status: "OK", message: "API is running" });
 });
 
-router.post("/employee", createEmployee);
 router.get("/employee", getEmployees);
-router.get("/employee/:id", getEmployeeById);
-router.get("/sorted_employee", getEmployeesSortedByName);
+router.get("/employee/sorted", getEmployeesSortedByName);
+router.post("/employee", createEmployee);
+
+// router.patch("/employee/", updateEmployee);
+// router.post("employee/deleteTry", deleteTry);
+// router.delete("/employee/", deleteEmployee);
+// router.get("/employee/name/:employeeName", getEmployeeByName);
+// router.get("/employee/name/DNI/:employeeDNI", getEmployeeById);
+// router.get("login", login);
+// router.get("logout", logout);
+// router.get("movement/:idEmpleado", getMovementsByIdEmployee);
+// router.get("movement", getMovements);
+// router.get("movementType", getMovementTypes);
+// router.get("position", getPositions);
+
+
+
+
+
 
 export default router;
