f1bd5e82eff6f8b96bf7b6c9f281187164da88a6
Brian Ramirez <Brianramirez01arias@gmail.com>
2025-04-15 00:16:22 -0600

    Adding employ Create and List controller

--- db-poc-api/src/controllers/Employee.controller.ts
+++ db-poc-api/src/controllers/Employee.controller.ts
@@ -1,59 +1,62 @@
 import { Request, Response } from "express";
 import EmployeeService from "../services/Employee.service";
+import {
+  GetEmployeesSuccessResponseDTO, 
+  EmployeesErrorResponseDTO,
+  CreateEmployeesDTO,
+  CreateEmployeesSuccessResponseDTO 
+} from "./../dtos/EmployeeDTO";
 
 export const createEmployee = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
   try {
-    const { Name, Salary } = req.body;
+    const data : CreateEmployeesDTO = req.body;
 
-    if (!Name || Name === "") {
-      res.status(400).json({ message: "Name is required" });
+    if (!data.NombreEmpleado || !data.ValorDocumentoIdentidad || !data.NombrePuesto) {
+      console.error("Request body is required.");
+      const errorResponse: EmployeesErrorResponseDTO = { success:false , code: 400, details: "Request body is required." };
+      res.status(400).json({ success: false, error: errorResponse });
       return;
     }
-    if (!Salary || Salary < 0) {
-      res.status(400).json({ message: "Valid money value is required" });
-      return;
-    }
-    
-    const response = await EmployeeService.createEmployee(Name, Salary);
 
-    res.status(201).json({ success: true, data: response });
+    const response = await EmployeeService.createEmployee(data);
+
+    if (response.success) {
+      const { success, data: employeeData } = response as CreateEmployeesSuccessResponseDTO;
+      res.status(201).json({ success, data: employeeData });
+    } else {
+      const { success, code, details } = response as EmployeesErrorResponseDTO;
+      res.status(500).json({ success, error: { code, details } });
+    }
   } catch (error) {
-    const errorMessage =
-      error instanceof Error ? error.message : "An unknown error occurred";
-    res.status(500).json({ success: false, error: errorMessage });
+    console.error("Error during employee creation:", error);
+    const errorMessage: EmployeesErrorResponseDTO = { success:false , code: 50008, details: "An error occurred while creating the employee" };
+    res.status(500).json({ success: errorMessage.success, error: { code: errorMessage.code, details: errorMessage.details } });
   }
 };
 
 export const getEmployees = async (
-  req: Request,
+  _req: Request,
   res: Response,
 ): Promise<void> => {
   try {
     const response = await EmployeeService.getEmployees();
-    res.status(200).json({ success: true, data: response });
+    if (response.success) {
+      const { success, data } = response as GetEmployeesSuccessResponseDTO;
+      res.status(200).json({ success, data });
+    } else {
+      const { success, code, details } = response as EmployeesErrorResponseDTO;
+      res.status(500).json({ success, error: { code, details } });
+    }
   } catch (error) {
-    const errorMessage =
-      error instanceof Error ? error.message : "An unknown error occurred";
-    res.status(500).json({ success: false, error: errorMessage });
+    console.error("Error during logout:", error);
+    res.status(500).json({ success: false, error: { code: 50008, details: "An error occurred while getting employees" } });
   }
 };
 
-export const getEmployeesSortedByName = async (
-  req: Request,
-  res: Response,
-): Promise<void> => {
-  try {
-    const response = await EmployeeService.getEmployeesSortedByName();
-    res.status(200).json({ success: true, data: response });
-  } catch (error) {
-    const errorMessage =
-      error instanceof Error ? error.message : "An unknown error occurred";
-    res.status(500).json({ success: false, error: errorMessage });
-  }
-};
+
 
 export const getEmployeeById = async (
   req: Request,
--- db-poc-api/src/controllers/Login.controller.ts
+++ db-poc-api/src/controllers/Login.controller.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 import LoginService from "../services/Auth.service";
-import  {LoginErrorResponseDTO,LoginDTO,LoginSuccessResponseDTO}  from "../dtos/LoginDTO";
+import  {LoginErrorResponseDTO,LoginDTO,LoginSuccessResponseDTO}  from "../dtos/AuthDTO";
+
 
 export async function loginUser(req: Request, res: Response): Promise<void> {
     try {
@@ -37,8 +38,29 @@ export async function loginUser(req: Request, res: Response): Promise<void> {
         }
         
     } catch (error) {
-        const errorMessage: LoginErrorResponseDTO = { success:false ,code: 500, details: "An error occurred while logging in" };
+        const errorMessage: LoginErrorResponseDTO = { success:false ,code: 50008, details: "An error occurred while logging in" };
         res.status(500).json({ success: errorMessage.success, error: {code: errorMessage.code,details : errorMessage.details} });
     }
+}
 
+export async function logoutUser(req: Request, res: Response): Promise<void> {
+    try {
+        const userId = req.body.UserId;
+        if (!userId) {
+            console.error("User ID is required.");
+            res.status(400).json({ success: false, error: { code: 400, details: "User ID is required." } });
+            return;
+        }
+
+        const response = await LoginService.logoutUser(userId);
+
+        if (response) {
+            res.status(200).json({ success: true, detail: "Logout successful" });
+        } else {
+            res.status(401).json({ success: false, error: { code: 401, details: "Logout failed" } });
+        }
+    } catch (error) {
+        console.error("Error during logout:", error);
+        res.status(500).json({ success: false, error: { code: 50008, details: "An error occurred while logging out" } });
+    }
 }
