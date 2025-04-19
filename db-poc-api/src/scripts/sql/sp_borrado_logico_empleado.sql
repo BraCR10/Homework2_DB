49fd3a553f59b63074ac9578b360db1ab98c5208
santivalverde4 <167050226+santivalverde4@users.noreply.github.com>
2025-04-19 12:42:54 -0600

    Agregación script proceso almacenado
    Se agrega el script del proceso almacenado para borrar lógicamente a un empleado.

---
+++ db-poc-api/src/scripts/sql/sp_borrado_logico_empleado.sql
@@ -0,0 +1,48 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Santiago Valverde>
+-- Create date: <19/04/2025>
+-- Description:	<Hace un borrado lógico del empleado, o sea, cambia el valor de activo a 0.>
+-- =============================================
+CREATE PROCEDURE sp_borrado_logico_empleado
+(
+    @inValorDocumentoIdentidad VARCHAR(16),
+    @outResultCode INT OUTPUT
+)
+AS
+BEGIN
+  SET NOCOUNT ON;
+  BEGIN TRY
+
+    -- Verificar existencia
+    IF NOT EXISTS (
+        SELECT 1 FROM Empleado 
+        WHERE ValorDocumentoIdentidad = @inValorDocumentoIdentidad AND EsActivo = 1
+    )
+    BEGIN
+      SET @outResultCode = 50004; --No existe el empelado
+      RETURN;
+    END
+
+    -- Realizar borrado lógico
+    UPDATE Empleado
+    SET EsActivo = 0
+    WHERE ValorDocumentoIdentidad = @inValorDocumentoIdentidad;
+
+    SET @outResultCode = 0;
+
+  END TRY
+  BEGIN CATCH
+    SET @outResultCode = 50008; --No se pudo actualizar correctamente.
+  END CATCH
+  SET NOCOUNT OFF;
+END
+GO
