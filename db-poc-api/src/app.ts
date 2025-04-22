b6a68aaef4744101eae667cd96193a2d147584d0
Brian Ramirez <Brianramirez01arias@gmail.com>
2025-04-22 12:52:16 -0600

    Changin app config

--- db-poc-api/src/app.ts
+++ db-poc-api/src/app.ts
@@ -1,16 +1,15 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import { initConnection } from "./config/db.config";
 import Router from "./routes";
 
 const app = express();
-export const useMock = true;
+export const useMock = process.env.USE_MOCK === "True" ? true : false;
 
-const allowedOrigins = [
-  "http://localhost:3001",
-  "http://localhost:3000",
-  
-];
+const allowedOrigins = process.env.NODE_ENV === 'production' 
+  ? ['http://localhost:3000', 'http://localhost:30001'] 
+  : ['http://localhost:3000', 'http://localhost:30001'];
+
 const corsOptions = {
   origin: allowedOrigins,
   methods: "GET,POST,PUT,DELETE,OPTIONS",
@@ -22,18 +21,33 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/v2", Router);
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  
+  const startServer = async (): Promise<void> => {
+    try {
+      await initConnection();
+      app.listen(PORT, () => {
+        console.log(`API running on port ${PORT}`);
+      });
+    } catch (error) {
+      console.error("Error details: ", error);
+      process.exit(1);
+    }
+  };
+  
+  startServer();
+}
 
-const startServer = async () => {
-  try {
-    await initConnection();
-    app.listen(PORT, () => {
-      console.log(`API running on http://localhost:${PORT}/api/v2`);
-    });
-  } catch (error) {
-    console.error("Error details: ", error);
-    process.exit(1);
+(async () => {
+  if (process.env.NODE_ENV === 'production') {
+    try {
+      await initConnection();
+      console.log('Database connection initialized for serverless environment');
+    } catch (error) {
+      console.error("Error initializing database connection:", error);
+    }
   }
-};
+})();
 
-startServer();
+export default app;
