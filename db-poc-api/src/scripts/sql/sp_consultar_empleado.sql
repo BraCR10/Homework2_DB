996d4074f998a563a666d98f6b1e43d1d817ef4e
santivalverde4 <167050226+santivalverde4@users.noreply.github.com>
2025-04-23 16:27:27 -0600

    Corrección consultar empleado

--- db-poc-api/src/scripts/sql/sp_consultar_empleado.sql
+++ db-poc-api/src/scripts/sql/sp_consultar_empleado.sql
@@ -14,31 +14,101 @@ GO
 -- =============================================
 CREATE PROCEDURE sp_consultar_empleado
 (
-    @inValorDocumentoIdentidad VARCHAR(16),
-    @outResultCode INT OUTPUT
+	@inFiltro VARCHAR(64),
+	@outResultCode INT OUTPUT
 )
 AS
 BEGIN
   SET NOCOUNT ON;
   BEGIN TRY
 
-    -- Buscar al empleado con datos requeridos
-    SELECT 
-      E.ValorDocumentoIdentidad,
-      E.Nombre,
-      P.Nombre AS NombrePuesto,
-      E.SaldoVacaciones
-    FROM dbo.Empleado E
-    INNER JOIN dbo.Puesto P ON E.IdPuesto = P.Id
-    WHERE 
-      E.ValorDocumentoIdentidad = @inValorDocumentoIdentidad AND
-      E.EsActivo = 1;
+    -- Limpieza de espacios en blanco
+    SET @inFiltro = LTRIM(RTRIM(@inFiltro));
+
+    -- Si el filtro está vacío, se listan todos los empleados activos
+    IF (@inFiltro = '')
+    BEGIN
+      SELECT
+	    E.Id,
+		E.IdPuesto,
+		P.Nombre AS NombrePuesto,
+		E.ValorDocumentoIdentidad,
+        E.Nombre,
+		E.FechaContratacion,
+        E.SaldoVacaciones,
+		E.EsActivo
+
+      FROM 
+        dbo.Empleado E
+        INNER JOIN dbo.Puesto P ON E.IdPuesto = P.Id
+      WHERE 
+        E.EsActivo = 1
+      ORDER BY 
+        E.Nombre ASC;
+
+      SET @outResultCode = 0;
+      RETURN;
+    END
+
+    -- Si el filtro contiene solo letras y espacios (búsqueda por nombre)
+    IF (@inFiltro LIKE '%[^0-9]%')
+    BEGIN
+      SELECT 
+        E.Id,
+		E.IdPuesto,
+		P.Nombre AS NombrePuesto,
+		E.ValorDocumentoIdentidad,
+        E.Nombre,
+		E.FechaContratacion,
+        E.SaldoVacaciones,
+		E.EsActivo
+      FROM 
+        dbo.Empleado E
+        INNER JOIN dbo.Puesto P ON E.IdPuesto = P.Id
+      WHERE 
+        E.EsActivo = 1
+        AND E.Nombre LIKE '%' + @inFiltro + '%'
+      ORDER BY 
+        E.Nombre ASC;
+
+      SET @outResultCode = 0;
+      RETURN;
+    END
+
+    -- Si el filtro contiene solo números (búsqueda por documento)
+    IF (@inFiltro NOT LIKE '%[^0-9]%')
+    BEGIN
+      SELECT
+		E.Id,
+		E.IdPuesto,
+		P.Nombre AS NombrePuesto,
+		E.ValorDocumentoIdentidad,
+        E.Nombre,
+		E.FechaContratacion,
+        E.SaldoVacaciones,
+		E.EsActivo
+      FROM 
+        dbo.Empleado E
+        INNER JOIN dbo.Puesto P ON E.IdPuesto = P.Id
+      WHERE 
+        E.EsActivo = 1
+        AND CAST(E.ValorDocumentoIdentidad AS VARCHAR) LIKE '%' + @inFiltro + '%'
+      ORDER BY 
+        E.Nombre ASC;
+
+      SET @outResultCode = 0;
+      RETURN;
+    END
 
     SET @outResultCode = 0;
-
   END TRY
-  BEGIN CATCH
-    SET @outResultCode = 50008; --No existe el empleado, entonces error en base de datos
+  BEGIN CATCH 
+	SET @outResultCode=50008; -- Error de base de datos
+
+	SELECT Descripcion AS detail
+	FROM dbo.Error
+	WHERE Codigo = @outResultCode;
+
   END CATCH
   SET NOCOUNT OFF;
 END
