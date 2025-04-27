5aa9b5704cfef72401a66e4c4982863978e474c6
Brian Ramirez <Brianramirez01arias@gmail.com>
2025-04-27 00:27:22 -0600

    Updating create applciation SP

--- db-poc-api/src/scripts/sql/sp_crear_solicitudes_vacaciones.sql
+++ db-poc-api/src/scripts/sql/sp_crear_solicitudes_vacaciones.sql
@@ -14,9 +14,9 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
+-- Author:		Brian Ramirez Arias
+-- Create date: 26/4/2025
+-- Description:	SP para crear solicitudes de vacaciones
 -- =============================================
 CREATE PROCEDURE sp_crear_solicitudes_vacaciones (
 	@inValorDocumentoIdentidad VARCHAR(64)
@@ -100,7 +100,7 @@ BEGIN
 		DECLARE @CantVacaciones INT;
 		SELECT @CantVacaciones = E.SaldoVacaciones
 		FROM dbo.Empleado AS E 
-		WHERE (E.ValorDocumentoIdentidad = @inValorDocumentoIdentidad);
+		WHERE (E.ValorDocumentoIdentidad = @inValorDocumentoIdentidad AND E.EsActivo = 1);
 
 		DECLARE @SaldoValidacion INT;
 		SET @SaldoValidacion = @CantVacaciones - @inCantDias;
@@ -109,6 +109,38 @@ BEGIN
 		IF (@SaldoValidacion<0)
 		BEGIN
 			SET @outResultCode = 50011; --  Monto invalido
+      
+			INSERT INTO dbo.DBError (
+					UserName
+					, Number
+					, Estado
+					, Severidad
+					, Linea
+					, ProcedureError
+					, Mensaje
+					, FechaHora
+				)
+				VALUES (
+					SUSER_NAME()
+					, ERROR_NUMBER()
+					, ERROR_STATE()
+					, ERROR_SEVERITY()
+					, ERROR_LINE()
+					, ERROR_PROCEDURE()
+					, ERROR_MESSAGE()
+					, GETDATE()
+				);
+
+			SELECT Descripcion AS detail
+			FROM dbo.Error
+			WHERE Codigo = @outResultCode;
+			RETURN;
+		END
+
+		-- Validacion emplado no activo
+		IF (@CantVacaciones IS NULL)
+		BEGIN
+			SET @outResultCode = 50008; --  Empleado no activo
       
 			INSERT INTO dbo.DBError (
 					UserName
@@ -168,6 +200,7 @@ BEGIN
 			WHERE Codigo = @outResultCode;
 			RETURN;
 		END
+
 		BEGIN TRANSACTION 
 
 			INSERT INTO dbo.Solicitud (
